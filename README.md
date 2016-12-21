fbf035ca18f1f93a9660f21cf4aea993107d0601
Beth Qiang <bethany.qiang@gmail.com>
2016-12-20 19:03:43 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Game of Life
+
+An implementation of [Conway's Game of Life](https://en.wikipedia.org/wiki/Conway's_Game_of_Life) using Sass, React, Redux, and (eventually) Immutable.js.
---
+++ src/store.js
@@ -0,0 +1,10 @@
+// import { createStore, applyMiddleware, combineReducers } from 'redux';
+// import createLogger from 'redux-logger';
+
+// const reducers = combineReducers({
+//   // reducers
+// });
+
+// const reduxMiddleware = applyMiddleware(createLogger());
+
+// export default createStore(reducers, reduxMiddleware);
